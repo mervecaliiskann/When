8ce53afc2e17f993792166277da187b933110481
MerveÇalışkan <mervecaliiskannk@gmail.com>
2024-11-13 10:09:26 +0300

    Remove unused PhoneNumber file
    Deleted the PhoneNumber file as it is no longer required in the project. This cleanup helps reduce clutter in the codebase and improves maintainability. All references to this file have been removed to ensure consistency.

---
+++ When/When/VİEW/PhoneNumberView.swift
@@ -0,0 +1,53 @@
+//
+//  PhoneNumberTextFieldView.swift
+//  When
+//
+//  Created by Merve Çalışkan on 18.09.2024.
+//
+
+import Foundation
+import UIKit
+
+final class PhoneNumberView: UIView {
+//    
+//    init() {
+//        super.init(frame: .zero)
+//        setupUI()
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    func setupUI() {
+//        
+//        
+//        let prefixTextField = UITextField()
+//        let phoneNumberTextView = UITextField()
+//        addSubview(prefixTextField)
+//        addSubview(phoneNumberTextView)
+//        prefixTextField.backgroundColor = .numberClr
+//        prefixTextField.layer.cornerRadius = 8
+//        prefixTextField.layer.borderWidth = 1
+//        prefixTextField.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            prefixTextField.topAnchor.constraint(equalTo: self.topAnchor),
+//            prefixTextField.leftAnchor.constraint(equalTo: self.leftAnchor),
+//            prefixTextField.heightAnchor.constraint(equalToConstant: 56),
+//            prefixTextField.widthAnchor.constraint(equalToConstant: 36)
+//        ])
+//
+//        phoneNumberTextView.placeholder = "Cep Telefonu"
+//        phoneNumberTextView.backgroundColor = .numberClr
+//        phoneNumberTextView.layer.cornerRadius = 8
+//        phoneNumberTextView.layer.borderWidth = 1
+//        phoneNumberTextView.translatesAutoresizingMaskIntoConstraints = false
+//        phoneNumberTextView.textAlignment = .left
+//        NSLayoutConstraint.activate([
+//            phoneNumberTextView.topAnchor.constraint(equalTo: self.topAnchor),
+//            phoneNumberTextView.rightAnchor.constraint(equalTo: self.rightAnchor),
+//            phoneNumberTextView.heightAnchor.constraint(equalToConstant: 56),
+//            phoneNumberTextView.leftAnchor.constraint(equalTo: prefixTextField.rightAnchor, constant: 16)
+//        ])
+//        
+//    }
+}
